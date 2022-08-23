98ab01449458ca431c5dcfc9be07bd1a7a4f59ea
lupawlik <lukaszpawlik609@gmail.com>
2022-08-23 19:33:28 +0200

    deploy heroku settings

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-k!_w4v+$g^-q0x9$6vba8w2(y%01+5pcq3%f4wa9lofxt4^+4("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-app0interview.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
